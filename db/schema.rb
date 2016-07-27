0f3ec86327564822dd4cf360a1e12c94e0497157
sanlouise <sandra.adamshallie@gmail.com>
2016-07-27 16:50:03 +0200

    Add plan model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160727131444) do
+ActiveRecord::Schema.define(version: 20160727144631) do
 
   create_table "members", force: true do |t|
     t.integer  "tenant_id"
@@ -40,6 +40,7 @@ ActiveRecord::Schema.define(version: 20160727131444) do
     t.string   "name"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "plan"
   end
 
   add_index "tenants", ["name"], name: "index_tenants_on_name"
