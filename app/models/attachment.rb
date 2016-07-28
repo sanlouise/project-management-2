1ec052ac6ac816c69eefbc5f1310cdd9cb074ff6
sanlouise <sandra.adamshallie@gmail.com>
2016-07-28 23:41:19 +0200

    Update attachment validations

--- app/models/attachment.rb
+++ app/models/attachment.rb
@@ -1,3 +1,19 @@
 class Attachment < ActiveRecord::Base
+
+	validates_presence_of :name, :upload
+  validates_uniqueness_of :name
+
+  validate :uploaded_file_size
   belongs_to :project
+
+  MAX_FILE_SIZE = 5.megabytes
+  attr_accessor :upload
+
+  private
+
+  	def uploaded_file_size
+  		return unless upload
+  		errors.add(:upload "File too big. Maximum 5MB.")
+  	end
+  
 end
