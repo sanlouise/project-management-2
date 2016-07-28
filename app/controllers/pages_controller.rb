19e0d860bd628763dfbf85024cff10cb3d5d41b5
sanlouise <sandra.adamshallie@gmail.com>
2016-07-28 22:11:46 +0200

    Update pages controller and form

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -12,6 +12,7 @@ class PagesController < ApplicationController
 			@tenant = Tenant.current_tenant
 			@projects = Project.by_user_plan_and_tenant(@tenant.id, current_user)
 			params[:tenant_id] = @tenant.id
+		end
 	end
 
 	def about
