5cce193bffcb84c7facde53c9b409ccc45a1f0bb
sanlouise <sandra.adamshallie@gmail.com>
2016-07-28 11:15:08 +0200

    Update pages controller

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,9 +2,19 @@ class PagesController < ApplicationController
   	skip_before_action :authenticate_tenant!, :only => [ :home, :about ]
 
 	def index
+		if current_user
+			if session[:tenant_id]
+				Tenant.set_current_tenant session[:tenant_id]
+			else 
+				Tenant.set_current_tenant current_user.tenants.first
+			end
+
+			@tenant = Tenant.current_tenant
+			params[:tenant_id] = @tenant.id
 	end
 
 	def about
 	end
 
 end
+
