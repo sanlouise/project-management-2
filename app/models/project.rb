1a5672092955c627c0836eed882d39690a3eed72
sanlouise <sandra.adamshallie@gmail.com>
2016-07-28 23:23:49 +0200

    Update project validations

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,16 +1,15 @@
 class Project < ActiveRecord::Base
   belongs_to :tenant
-  validates_uniqueness_of :title
+  validates_presence_of :title
+  validates_presence_of :details
+  validates_presence_of :deadline
   validate :free_plan_can_only_have_five_projects
 
   def free_plan_can_only_have_five_projects
-
   	if self.new_record? && (tenant.projects.count > 5) && (tenant.plan == 'free')
   		errors.add(:base, "Free plans cannot have more than 5 projects.")
   	end
-
     Project.by_user_plan_and_tenant(tenant.id)
-
   end
 
   private
