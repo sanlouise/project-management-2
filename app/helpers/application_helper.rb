ceafa88e734a7bf9cbc05a5e0ba00ec735c6ae20
sanlouise <sandra.adamshallie@gmail.com>
2016-07-29 11:44:36 +0200

    Update application helper for attachment uploads

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,5 +3,9 @@ module ApplicationHelper
 	def tenant_name(tenant_id)
 		Tenant.find(tenant_id).name
 	end
+  
+  def s3_link(tenant_id, attachment_key)
+    link_to attachment_key, "#{attachment_key}", class: "main-link", target: 'new'
+  end
 
 end
