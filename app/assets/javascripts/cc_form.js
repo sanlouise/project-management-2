9ac6743e9bf846efe5ad7881b55d7bc61e2c4788
sanlouise <sandra.adamshallie@gmail.com>
2016-07-30 13:30:08 +0200

    Add comments to cc_form JS

--- app/assets/javascripts/cc_form.js
+++ app/assets/javascripts/cc_form.js
@@ -1,3 +1,4 @@
+//Get params from URL to determine whether the plan is premium or basic
 function GetURLParameter(sParam) {
   var sPageURL = window.location.search.substring(1);
   var sURLVariables = sPageURL.split('&');
@@ -9,30 +10,35 @@ function GetURLParameter(sParam) {
   }
 };
 
+//Handle submission of form and intercept default
 $(document).ready(function () {
-  
   var show_error, stripeResponseHandler, submitHandler;
+
   submitHandler = function (event) {
     var $form = $(event.target);
+    //Ensure that submit button cannot be clicked multiple times
     $form.find("input[type=submit]").prop("disabled", true);
-    if(Stripe){
+    //Stripe returns a token (from documentation)
+    if(Stripe) {
       Stripe.card.createToken($form, stripeResponseHandler);
     } else {
-      show_error("Failed to load credit card processing functionality. Please reload the page")
+      show_error("Failed to process your credit card information. Please reload the page")
     }
+    //Prevent default from submitting
     return false;
   };
 
+  //Submit handler listener for form
   $(".cc_form").on('submit', submitHandler);
 
-
+  //Change plan event listener
   var handlePlanChange = function(plan_type, form) {
     var $form = $(form);
     
     if(plan_type == undefined) {
       plan_type = $('#tenant_plan :selected').val();
     }
-    
+    //When premium plan is selected, show credit card fields
     if( plan_type === 'premium') {
       $('[data-stripe]').prop('required', true);
       $form.off('submit');
@@ -45,25 +51,23 @@ $(document).ready(function () {
     }
   }
 
-
   $("#tenant_plan").on('change', function(event) {
     handlePlanChange($('#tenant_plan :selected').val(), ".cc_form");
   });
 
-
   handlePlanChange(GetURLParameter('plan'), ".cc_form");
-
 
   stripeResponseHandler = function (status, response) {
     var token, $form;
-    
     $form = $('.cc_form');
     
     if (response.error) {
-      console.log(response.error.message);
+      console.log(response.error.message)
       show_error(response.error.message);
+      //Enable signup button again
       $form.find("input[type=submit]").prop("disabled", false);
     } else {
+      // Removing data from fields after submission, from Stripe ducumentation
       token = response.id;
       $form.append($("<input type=\"hidden\" name=\"payment[token]\" />").val(token));
       $("[data-stripe=number]").remove();
@@ -73,9 +77,11 @@ $(document).ready(function () {
       $("[data-stripe=label]").remove();
       $form.get(0).submit();
     }
+    //Stops default
     return false;
   };
-  // 
+
+  // Errors when Stripe returns an error
   show_error = function (message) {
     if($("#flash-messages").size() < 1){
       $('div.container.main div:first').prepend("<div id='flash-messages'></div>")
