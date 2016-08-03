80dedd604d1653813a4e27e690c81b9bec82e742
sanlouise <sandra.adamshallie@gmail.com>
2016-08-03 15:53:42 +0200

    Update routes for plan upgrades

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   
+  get 'pages/home'
+  get 'pages/about'
+
   resources :attachments
   resources :tenants do
     resources :projects
@@ -8,9 +11,10 @@ Rails.application.routes.draw do
   resources :members
   root :to => "pages#home"
 
-    
   as :user do   
     match '/user/confirmation' => 'confirmations#update', :via => :put, :as => :update_user_confirmation
+    match '/plan/edit' => 'tenants#edit', via: :get, as: :edit_plan
+    match '/plan/update' => 'tenants#update', via: [:put, :patch], as: :update_plan
   end
 
   devise_for :users, :controllers => { 
@@ -19,9 +23,5 @@ Rails.application.routes.draw do
     :sessions => "milia/sessions", 
     :passwords => "milia/passwords", 
   }
-
-  get 'pages/home'
-  get 'pages/about'
-  match '/plan/edit' => 'tenants#edit', via: :get, as: :edit_plan
   
 end
