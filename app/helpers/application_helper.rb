e7a8d8baa6968cd2cfeeb6b5cfa57f4351c7973b
sanlouise <sandra.adamshallie@gmail.com>
2016-08-03 15:52:31 +0200

    Add helper method for form to update tenant plan

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,10 @@
 module ApplicationHelper
 
+	def class_name_for_tenant_form(tenant)
+    return "cc_form" if tenant.payment.blank?
+    ""
+  end
+
 	def tenant_name(tenant_id)
 		Tenant.find(tenant_id).name
 	end
