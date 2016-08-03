061d0fc531c9f2b755368718100c142870312684
sanlouise <sandra.adamshallie@gmail.com>
2016-08-04 00:40:58 +0200

    Clean up attachments controller

--- app/controllers/attachments_controller.rb
+++ app/controllers/attachments_controller.rb
@@ -1,37 +1,26 @@
 class AttachmentsController < ApplicationController
 
-  before_action :set_attachment, only: [:show, :edit, :update, :destroy]
+  before_action :set_attachment, only: [:show, :destroy]
 
-  # GET /attachments
-  # GET /attachments.json
   def index
     @attachments = Attachment.all
   end
 
-  # GET /attachments/1
-  # GET /attachments/1.json
   def show
   end
 
-  # GET /attachments/new
   def new
     @attachment = Attachment.new
     @attachment.project_id = params[:project_id]
   end
 
-  # GET /attachments/1/edit
-  def edit
-  end
-
-  # POST /attachments
-  # POST /attachments.json
   def create
     @attachment = Attachment.new(attachment_params)
 
     respond_to do |format|
       if @attachment.save
         format.html { redirect_to tenant_project_url(tenant_id: Tenant.current_tenant_id, id: @attachment.project_id),
-        notice: 'Attachment was created successfully .' }
+        notice: 'Attachment was created successfully.' }
         format.json { render :show, status: :created, location: @attachment }
       else
         format.html { render :new }
@@ -40,38 +29,20 @@ class AttachmentsController < ApplicationController
     end
   end
 
-  # PATCH/PUT /attachments/1
-  # PATCH/PUT /attachments/1.json
-  def update
-    respond_to do |format|
-      if @attachment.update(attachment_params)
-        format.html { redirect_to tenant_project_url(tenant_id: Tenant.current_tenant_id, id: @attachment.project_id), 
-        notice: 'Attachment was updated successfully .' }
-        format.json { render :show, status: :ok, location: @attachment }
-      else
-        format.html { render :edit }
-        format.json { render json: @attachment.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /attachments/1
-  # DELETE /attachments/1.json
   def destroy
     @attachment.destroy
     respond_to do |format|
-      format.html { redirect_to attachments_url, notice: 'Attachment was successfully destroyed.' }
+      format.html { redirect_to tenant_project_url(tenant_id: Tenant.current_tenant_id, id: @attachment.project_id), notice: 'Attachment was successfully destroyed.' }
       format.json { head :no_content }
     end
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
+
     def set_attachment
       @attachment = Attachment.find(params[:id])
     end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
     def attachment_params
       params.require(:attachment).permit(:name, :project_id, :upload)
     end
