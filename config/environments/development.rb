471720175806f3bf0d373684fce080b92e647c5a
sanlouise <sandra.adamshallie@gmail.com>
2016-08-19 12:32:45 +0200

    Minor fixes for user confirmation email settings

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -13,7 +13,7 @@ Rails.application.configure do
     :address => "smtp.gmail.com",
     :port => "587",
     :authentication => :plain,
-    :user_name => "do-not-reply@example.com",
+    :user_name => "do-not-reply@dataflow.com",
     :password => ENV["SMTP_ENTRY"],
     :enable_starttls_auto => true
   }
