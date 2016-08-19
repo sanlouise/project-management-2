80c9c24191d6a971a48361f899f5f41a4bc96038
sanlouise <sandra.adamshallie@gmail.com>
2016-08-19 12:24:29 +0200

    Minor fix registrations controller

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -15,7 +15,7 @@ class RegistrationsController < Milia::RegistrationsController
       # have a working copy of the params in case Tenant callbacks
       # make any changes
     tenant_params = sign_up_params_tenant
-    user_params   = sign_up_params_user.merge({ is_admin:true })
+    user_params   = sign_up_params_user.merge({ is_admin: true })
     coupon_params = sign_up_params_coupon
 
     sign_out_session!
@@ -41,7 +41,7 @@ class RegistrationsController < Milia::RegistrationsController
               flash[:error] = e.message
               @tenant.destroy
               log_action("Payment failed")
-              # render :new and return
+              render :new and return
             end
           end
         else
